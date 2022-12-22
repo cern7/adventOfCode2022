d9abfe443f1418bb0691cce1097331df2e7f4b17
cern7 <62252282+cern7@users.noreply.github.com>
2022-12-22 18:02:36 +0200

    Solution for Day4 part1

---
+++ Day4/src/Day4.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.LinkPermission;
+
+public class Day4 {
+    private static final String inputPath = "inputDay4.txt";
+    private static int assgPair;
+
+    private static void sol1() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(inputPath));
+        String line;
+
+        while (reader.ready()) {
+            line = reader.readLine();
+
+            Integer firstElfStart = Integer.parseInt(line.split(",")[0].split("-")[0]);
+            Integer firstElfEnd = Integer.parseInt(line.split(",")[0].split("-")[1]);
+            Integer secondElfStart = Integer.parseInt(line.split(",")[1].split("-")[0]);
+            Integer secondElfEnd = Integer.parseInt(line.split(",")[1].split("-")[1]);
+            if (firstElfStart <= secondElfStart && firstElfEnd >= secondElfEnd) {
+                assgPair++;
+            } else if (secondElfStart <= firstElfStart && secondElfEnd >= firstElfEnd) {
+                assgPair++;
+            }
+//            break;
+        }
+        System.out.println(assgPair);
+    }
+
+    public static void main(String[] args) throws IOException {
+        sol1();
+    }
+
+}
